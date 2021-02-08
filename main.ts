93d07a4961b531a91016e5db19b853a30876f16f
tss-smart <78737629+tss-smart@users.noreply.github.com>
2021-02-08 21:10:22 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hello!")
+    basic.pause(1000)
+    curTime += 1
+})
+let DRINK_WATER_PERIOD = 3600
+let 
+let curTime = input.runningTime()
+basic.forever(function () {
+	
+})
