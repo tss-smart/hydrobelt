42c2388858ad368797ee1ab5bfdb47f501be07a1
tss-smart <78737629+tss-smart@users.noreply.github.com>
2021-02-08 21:48:09 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,31 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("Hello!")
     basic.pause(1000)
-    curTime += 1
+    nowTime = input.runningTime()
 })
-let DRINK_WATER_PERIOD = 3600
-let 
-let curTime input.runningTime()
+let STATE_GREET = 0
+let nowTime = 0
+let amountDrunk = 0
+nowTime = input.runningTime()
+let USER_TYPE_CHILD = 1
+let USER_TYPE_ADULT = 2
+let USER_TYPE_ELDERLY = 3
+let DRINK_PERIOD_CHILD = 2700
+let DRINK_PERIOD_ADULT = 3600
+let DRINK_PERIOD_ELDERY = 2700
+let DRINK_EACH_TIME_CHILD = 200
+let DRINK_EACH_TIME_ADULT = 250
+let DRINK_EACH_TIME_ELDERY = 200
+let DRINK_AMOUNT_CHILD = 1500
+let DRINK_AMOUNT_ADULT = 2000
+let DRINK_AMOUNT_ELDERY = 2000
+let STATE_SHOW_USER_TYPE = 1
+let STATE_SHOW_AMOUNT_DRINK = 2
+let STATE_WAITING = 3
+let userType = USER_TYPE_CHILD
+let nextRemindtime = nowTime + DRINK_PERIOD_CHILD
+let state = STATE_GREET
 basic.forever(function () {
-	
+    if (state == STATE_GREET) {
+        basic.showString("HydroBelt")
+    }
 })
