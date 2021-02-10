c8122b6fa7314dc8949c98c39190b8d65ebbf210
tss-smart <78737629+tss-smart@users.noreply.github.com>
2021-02-10 23:26:49 +0800

    Now support remind sound

--- main.ts
+++ main.ts
@@ -1,5 +1,34 @@
 
+let STATE_GREET = 0
+let STATE_SHOW_USER_TYPE = 1
+let STATE_SHOW_AMOUNT_DRINK = 2
+let STATE_WAITING = 3
+let STATE_SHOW_SCREEN_SAVER = 4
+let STATE_REMIND_DRINK = 5
+let STATE_SHOW_GOAL_ACHIEVED = 6
+let STATE_SHOW_GOAL_ACHIEVED_WAITING = 7
 
+let USER_TYPE_CHILD = 0
+let USER_TYPE_ADULT = 1
+let USER_TYPE_ELDERLY = 2
+
+let USER_TYPE_STRS = ["Child", "Adult", "Elderly"]
+
+let DRINK_GOAL_AMOUNTS = [1600, 2000, 2000]
+let DRINK_EACH_TIMES = [200, 250, 200]
+
+// let DRINK_PERIODS = [2700, 3600, 2700] // in seconds
+let DRINK_PERIODS = [2700, 3600, 2700] // in seconds
+
+let SCREEN_SAVER_PERIOD = 10 // in seconds
+
+let userType = USER_TYPE_CHILD
+let nextRemindTime = 0
+let nextScreenSaveTime = 0
+let nextShowGoalAchievedTime = 0
+let nextBackWaitingTime = 0
+let state = 0
+let amountDrunk = 0
 
 input.onButtonPressed(Button.A, function () {
     if (state == STATE_WAITING) {
@@ -14,6 +43,7 @@ input.onButtonPressed(Button.A, function () {
         state = STATE_SHOW_USER_TYPE
     }
 })
+
 input.onButtonPressed(Button.B, function () {
     if (state == STATE_WAITING) {
         amountDrunk -= DRINK_EACH_TIMES[userType]
@@ -23,6 +53,7 @@ input.onButtonPressed(Button.B, function () {
     }
     
 })
+
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     if (state == STATE_WAITING) {
         userType += 1
@@ -31,40 +62,6 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         state = STATE_SHOW_USER_TYPE
     }
 })
-
-let amountDrunk = 0
-
-let USER_TYPE_CHILD = 0
-let USER_TYPE_ADULT = 1
-let USER_TYPE_ELDERLY = 2
-
-let USER_TYPE_STRS = ["Child", "Adult", "Elderly"]
-
-let DRINK_GOAL_AMOUNTS = [1600, 2000, 2000]
-
-let DRINK_EACH_TIMES = [200, 250, 200]
-
-// let DRINK_PERIODS = [2700, 3600, 2700] // in seconds
-let DRINK_PERIODS = [10, 3600, 2700] // in seconds
-
-let SCREEN_SAVER_PERIOD = 10 // in seconds
-
-let userType = USER_TYPE_CHILD
-let nextRemindTime = 0
-let nextScreenSaveTime = 0
-let nextShowGoalAchievedTime = 0
-
-let tmp 
-let state = 0
-
-let STATE_GREET = 0
-let STATE_SHOW_USER_TYPE = 1
-let STATE_SHOW_AMOUNT_DRINK = 2
-let STATE_WAITING = 3
-let STATE_SHOW_SCREEN_SAVER = 4
-let STATE_REMIND_DRINK = 5
-let STATE_SHOW_GOAL_ACHIEVED = 6
-let STATE_SHOW_GOAL_ACHIEVED_WAITING = 7
 
 basic.forever(function () {
     if (state == STATE_GREET) {
@@ -81,22 +78,18 @@ basic.forever(function () {
         nextRemindTime = input.runningTime() + DRINK_PERIODS[userType]*1000
         state = STATE_WAITING
     } else if (state == STATE_WAITING) {
-        if (input.runningTime()>nextRemindTime) 
+        if (input.runningTime()>nextScreenSaveTime)
+        {
+            state=STATE_SHOW_SCREEN_SAVER
+        } else if (input.runningTime()>nextRemindTime) 
         {
             state=STATE_REMIND_DRINK
         }
-        else if (input.runningTime()>nextScreenSaveTime)
-        {
-            state=STATE_SHOW_SCREEN_SAVER
-        }
-    } else if (state == STATE_REMIND_DRINK)
-    {
+    } else if (state == STATE_REMIND_DRINK) {
         music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
         nextRemindTime += DRINK_PERIODS[userType]*1000
-        state=STATE_WAITING
-    } else if (state == STATE_SHOW_SCREEN_SAVER)
-    {
-
+        state=STATE_WAITING       
+    } else if (state == STATE_SHOW_SCREEN_SAVER) {
 
     } else if (state == STATE_SHOW_GOAL_ACHIEVED) {
         basic.clearScreen()
@@ -104,10 +97,10 @@ basic.forever(function () {
         basic.showIcon(IconNames.Happy)
         nextShowGoalAchievedTime = input.runningTime() + 2000 
         state = STATE_SHOW_GOAL_ACHIEVED_WAITING
-    } else if (state == STATE_SHOW_GOAL_ACHIEVED_WAITING)
-    {
+    } else if (state == STATE_SHOW_GOAL_ACHIEVED_WAITING) {
         if (input.runningTime()>=nextShowGoalAchievedTime)
             state = STATE_SHOW_GOAL_ACHIEVED
     }
 })
+
 
